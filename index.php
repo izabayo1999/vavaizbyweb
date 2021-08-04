90cfa1a2c0f1e644f621cbeec55f58c7592ac81b
izabayo1999 <87212079+izabayo1999@users.noreply.github.com>
2021-08-04 13:24:31 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>Home Page - Online Death Registration</title>
+
+
+    <!-- Core Stylesheet -->
+    <link href="style.css" rel="stylesheet">
+
+    <!-- Responsive CSS -->
+    <link href="css/responsive/responsive.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <!-- Preloader Start -->
+    <div id="preloader">
+        <div class="loader">
+            <span class="inner1"></span>
+            <span class="inner2"></span>
+            <span class="inner3"></span>
+        </div>
+    </div>
+
+
+
+    <!-- ***** Header Area Start ***** -->
+    <header class="header_area" id="header">
+        <div class="container-fluid h-100">
+            <div class="row h-100">
+                <div class="col-12 h-100">
+                    <nav class="h-100 navbar navbar-expand-lg align-items-center">
+                        <a class="navbar-brand" href="index.php">Online Death Registration</a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#fancyNav"
+                            aria-controls="fancyNav" aria-expanded="false" aria-label="Toggle navigation"><span
+                                class="ti-menu"></span></button>
+                        <div class="collapse navbar-collapse" id="fancyNav">
+                            <ul class="navbar-nav ml-auto">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                                </li>
+
+                                <li class="nav-item">
+                                    <a class="nav-link" href="user/login.php">User</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="admin/login.php">Admin</a>
+                                </li>
+
+                            </ul>
+
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ***** Header Area End ***** -->
+
+    <!-- ***** Hero Area Start ***** -->
+    <div class="fancy-hero-area bg-img bg-overlay animated-img"
+        style="background-image: url(img/bg-img/1140-quigley-millenials-in-mourning.jpg);">
+        <!-- ***** Hero Area End ***** -->
+
+        <div class="fancy-hero-title">
+            <div class="hero container">
+                <h1>Welcome to Death Registration Platform</h1>
+            </div>
+        </div>
+
+        <div class="fancy-hero-services">
+            <div class="service container">
+                <div class="service-top">
+                    <h4 class="section-title">What We provide</h4>
+                    <p><b>A well developed and functioning civil registration system ensures the registration of all
+                            vital
+                            events including births,and deaths and issues relevant certificates as proof of such
+                            registration.
+                            Civil registration promotes efficient government planning, effective use of resources and
+                            aid
+                            and more accurate monitoring of progress towards achieving the sustainable development
+                            plan.<br>
+                    </p><br>
+                    <p> Death certificates are used to compile mortality statistics that are used to assess health, set
+                        public health goals and policies, and direct research and resources. The death certificate
+                        contains
+                        vital information about the deceased, as well as the circumstances surrounding his or her death
+                        and
+                        the cause of death.</b></p><br>
+                </div>
+            </div>
+        </div>
+
+        <!-- ***** Footer Area Start ***** -->
+        <footer class="fancy-footer-area fancy-bg-dark">
+
+            <!-- Footer Copywrite -->
+            <div class="footer-copywrite-area">
+                <div class="container h-100">
+                    <div class="row h-100">
+                        <div class="col-12 h-100">
+                            <div class="copywrite-content h-100 d-flex align-items-center justify-content-between">
+                                <!-- Copywrite Text -->
+                                <div class="copywrite-text">
+                                    <p>
+                                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                                        Copyright &copy;<script>
+                                        document.write(new Date().getFullYear());
+                                        </script> | Online Death Registration System
+                                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                                    </p>
+                                </div>
+
+                                <div class="text-muted">
+                                    +25 0789 820 905 / Hotline: 0001
+                                </div>
+                                <!-- Footer Nav -->
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- ***** Footer Area End ***** -->
+
+        <!-- jQuery-2.2.4 js -->
+        <script src="js/jquery/jquery-2.2.4.min.js"></script>
+        <!-- Popper js -->
+        <script src="js/bootstrap/popper.min.js"></script>
+        <!-- Bootstrap-4 js -->
+        <script src="js/bootstrap/bootstrap.min.js"></script>
+        <!-- All Plugins js -->
+        <script src="js/others/plugins.js"></script>
+        <!-- Active JS -->
+        <script src="js/active.js"></script>
+</body>
